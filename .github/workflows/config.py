2b434738f7c026584c9adcc9b82c7682da1292a0
wrgok <72342335+wrgok@users.noreply.github.com>
2022-06-14 13:58:05 +0800

    Update config.py

--- .github/workflows/config.py
+++ .github/workflows/config.py
@@ -3,4 +3,4 @@ SIGN_URL = 'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_sy
 #签到的COOKIE
 COOKIE = 'tvfe_boss_uuid=3a487b102e4837d2; video_platform=2; pgv_info=ssid=s8025013888; pgv_pvid=6564531778; _qpsvr_localtk=0.9471925782762107; video_guid=15588ae5c3d9a55ee12864ee002affd5; RK=0QPtBbEUbg; ptcz=9a610f13b5cb59c1d7b45d66b06287a43af1c785cb7f7f326588611f01ac25d6; main_login=qq; vqq_access_token=F3AD7F8AE9EDFCEAE468B74EB70BF14E; vqq_appid=101483052; vqq_openid=3597B24BBEE1EA9B8956D40E655844E4; vqq_vuserid=197501964; vqq_vusession=W1OwTMIifCT-WPM8k8oN0A.N; vqq_refresh_token=3FF3A953E03A0025287ABCF5DA619CE6; login_time_init=2022-6-14 13:20:2; vqq_next_refresh_time=6563; vqq_login_time_init=1655184104; login_time_last=2022-6-14 13:20:30'
 #PUSHPLUS的TOKEN
-TOKEN = ''
+TOKEN = '792391b3bccf475c9f729a89c2e52250'
