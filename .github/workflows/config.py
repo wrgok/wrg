1593c53f381773afcd5887b8461e2c1bf627f2d1
wrgok <72342335+wrgok@users.noreply.github.com>
2022-06-14 13:25:49 +0800

    Update config.py

--- .github/workflows/config.py
+++ .github/workflows/config.py
@@ -1,6 +1,7 @@
 #签到的URL
 SIGN_URL = 'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_system&cmd=2'
 #签到的COOKIE
-COOKIE = ''
+COOKIE = 'tvfe_boss_uuid=3a487b102e4837d2; video_platform=2; pgv_info=ssid=s8025013888; pgv_pvid=6564531778; _qpsvr_localtk=0.9471925782762107; video_guid=15588ae5c3d9a55ee12864ee002affd5; RK=0QPtBbEUbg; ptcz=9a610f13b5cb59c1d7b45d66b06287a43af1c785cb7f7f326588611f01ac25d6; main_login=qq; vqq_access_token=F3AD7F8AE9EDFCEAE468B74EB70BF14E; vqq_appid=101483052; vqq_openid=3597B24BBEE1EA9B8956D40E655844E4; vqq_vuserid=197501964; vqq_vusession=W1OwTMIifCT-WPM8k8oN0A.N; vqq_refresh_token=3FF3A953E03A0025287ABCF5DA619CE6; login_time_init=2022-6-14 13:20:2; vqq_next_refresh_time=6563; vqq_login_time_init=1655184104; login_time_last=2022-6-14 13:20:30'
 #PUSHPLUS的TOKEN
 TOKEN = ''
+main_login=qq; vqq_access_token=63A23033CB08EE3E888B1C1C6A9BE880; vqq_appid=101527197; vqq_login_time_init=1643045022; vqq_next_refresh_time=6555; vqq_openid=D43564542C8B11A1FAB28434ABBF845A; vqq_refresh_token=59A0E26BED43B920973E30A57A1AEB50; vqq_vuserid=197501964; vqq_vusession=93a9FB8gDYVS1pOVaFXZzQ..; pgv_info=ssid=s9143764052; pgv_pvid=6216419096; video_platform=2; tvfe_boss_uuid=c76b10f33d5b2c4f; video_guid=10e4f0c6b304507a; o_cookie=275643922; ptcz=33c2c05371f4e0736c366fd9dd543c759ad41bdb14dc136a65151706f04a04bd; iip=0; pac_uid=1_261319993; RK=ZYLlV5E+PC
